00d9e45ab23960e3eb48920afb1ebc7dc65209f4
Zamanof <Zamanov@itstep.edu.az>
2026-01-19 21:07:27 +0400

    Pagination end

--- ASP NET 06. Student MVC - Pagination Filtering Ordering/Controllers/StudentsController.cs
+++ ASP NET 06. Student MVC - Pagination Filtering Ordering/Controllers/StudentsController.cs
@@ -3,159 +3,197 @@ using ASP_NET_06._Student_MVC___Pagination_Filtering_Ordering.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ASP_NET_06._Student_MVC___Pagination_Filtering_Ordering.Controllers
+namespace ASP_NET_06._Student_MVC___Pagination_Filtering_Ordering.Controllers;
+
+public class StudentsController : Controller
 {
-    public class StudentsController : Controller
+    private readonly StudentsDbContext _context;
+
+    public StudentsController(StudentsDbContext context)
     {
-        private readonly StudentsDbContext _context;
+        _context = context;
+    }
 
-        public StudentsController(StudentsDbContext context)
+    // GET: Students
+    public async Task<IActionResult> Index(
+        int page = 1,
+        int pageSize=4,
+        string nameFilter = null!,
+        string lastFilter = null!,
+        string orderingBy="Name",
+        string orderingDirection="asc"
+        )
+    {
+        IQueryable<Student> studentsQuery = _context.Students;
+
+        // filter
+        if (!string.IsNullOrWhiteSpace(nameFilter))
         {
-            _context = context;
+            studentsQuery = studentsQuery.Where(s => s.FirstName.Contains(nameFilter));
         }
 
-        // GET: Students
-        public async Task<IActionResult> Index(
-            int page = 1,
-            int pageSize=4)
+        if (!string.IsNullOrWhiteSpace(lastFilter))
         {
-            IQueryable<Student> studentsQuery = _context.Students;
-
-            // Pagination
-            var data = await studentsQuery
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToListAsync();
-            return View(data);
+            studentsQuery = studentsQuery.Where(s => s.LastName.Contains(lastFilter));
         }
 
-        // GET: Students/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // oredring
+        if(orderingDirection == "asc")
         {
-            if (id == null)
+            studentsQuery = orderingBy switch
             {
-                return NotFound();
-            }
-
-            var student = await _context.Students
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
+                "Name" => studentsQuery.OrderBy(s => s.FirstName),
+                "LastName" => studentsQuery.OrderBy(s => s.LastName)
+            };
         }
 
-        // GET: Students/Create
-        public IActionResult Create()
+        else if (orderingDirection == "desc")
         {
-            return View();
+            studentsQuery = orderingBy switch
+            {
+                "Name" => studentsQuery.OrderByDescending(s => s.FirstName),
+                "LastName" => studentsQuery.OrderByDescending(s => s.LastName)
+            };
         }
 
-        // POST: Students/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName")] Student student)
+        var count = await _context.Students.CountAsync();
+
+        // Pagination
+        var data = await studentsQuery
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        var view = new PginationViewModel<Student>(data, page, pageSize, count);
+        return View(view);
+    }
+
+    // GET: Students/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(student);
+            return NotFound();
         }
 
-        // GET: Students/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (student == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var student = await _context.Students.FindAsync(id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            return View(student);
+            return NotFound();
         }
 
-        // POST: Students/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName")] Student student)
+        return View(student);
+    }
+
+    // GET: Students/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Students/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,FirstName,LastName")] Student student)
+    {
+        if (ModelState.IsValid)
         {
-            if (id != student.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(student);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StudentExists(student.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(student);
-        }
-
-        // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var student = await _context.Students
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var student = await _context.Students.FindAsync(id);
-            if (student != null)
-            {
-                _context.Students.Remove(student);
-            }
-
+            _context.Add(student);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(student);
+    }
 
-        private bool StudentExists(int id)
+    // GET: Students/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
         {
-            return _context.Students.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+        return View(student);
+    }
+
+    // POST: Students/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName")] Student student)
+    {
+        if (id != student.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(student.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(student);
+    }
+
+    // GET: Students/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+
+    // POST: Students/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool StudentExists(int id)
+    {
+        return _context.Students.Any(e => e.Id == id);
     }
 }
---
+++ ASP NET 06. Student MVC - Pagination Filtering Ordering/Models/PginationViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ASP_NET_06._Student_MVC___Pagination_Filtering_Ordering.Models;
+
+public class PginationViewModel<TModel>
+{
+    public IEnumerable<TModel> Items  { get;}
+    public int Page { get;}
+    public int PageSize { get;}
+    public int TotalCount { get;}
+
+    public PginationViewModel(
+        IEnumerable<TModel> items, 
+        int page, 
+        int pageSize, 
+        int count)
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = Convert.ToInt32(Math.Ceiling((float)count / pageSize));
+        // 12/5 = 2.4 => 3
+    }
+}
